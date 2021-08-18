b8b061da2c25984dbc9687a01a72d60eaba89d1d
Ehsan Dastras <ehsan1372d@gmail.com>
2021-08-18 01:00:58 -0700

    Add Delete Property Test

--- tests/Feature/PropertyTest.php
+++ tests/Feature/PropertyTest.php
@@ -14,13 +14,15 @@ class PropertyTest extends TestCase
      */
     public function a_property_could_be_stored()
     {
-        $this->withoutExceptionHandling();
+       // $this->withoutExceptionHandling();
         $response =$this->post('/property',[
            'title'=>'Appartment',
            'price'=>25000,
         ]);
-        $response->assertOk();
+        $property=Property::first();
         $this->assertCount(1,Property::all());
+        //path is defined function in property model
+        $response->assertRedirect($property->path());
     }
     /**
      * @test
@@ -53,7 +55,7 @@ class PropertyTest extends TestCase
      */
     public function a_property_could_be_updated()
     {
-        $this->withoutExceptionHandling();
+       // $this->withoutExceptionHandling();
         //first store
         $this->post('/property',[
             'title'=>'Appartment',
@@ -63,12 +65,37 @@ class PropertyTest extends TestCase
         $property=Property::first();
 
         //update store
-        $response=$this->patch('/property/'.$property->id,[
+        $response=$this->patch($property->fresh()->path(),[
             'title'=>'home',
             'price'=>300,
         ]);
         $this->assertEquals('home',Property::first()->title);
         $this->assertEquals('300',Property::first()->price);
+
+        $response->assertRedirect($property->path());
     }
+    /**
+     * @test
+     */
+    public function a_property_could_be_deleted()
+    {
+        //$this->withoutExceptionHandling();
+
+        $this->post('/property',[
+            'title'=>'Appartment',
+            'price'=>2500
+        ]);
+        $property =Property::first();
+        $this->assertCount(1,Property::all());
+
+        $response=$this->delete($property->path());
+
+        $this->assertCount(0,Property::all());
+
+        //Return to ...
+        $response->assertRedirect('/properties');
+
+    }
+
 }
 
